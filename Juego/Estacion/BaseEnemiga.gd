1ef0d4d501c6e11cadadff228a83ecb7b8a57bcf
Federico Benítez <benitez.federico@gmail.com>
2022-10-30 18:34:28 -0300

    orbital final2

--- Juego/Estacion/BaseEnemiga.gd
+++ Juego/Estacion/BaseEnemiga.gd
@@ -2,19 +2,23 @@ class_name BaseEnemiga
 extends Node2D
 
 export var hitpoints:float = 10.0
-export var orbital:PackedScene = null
 export var numero_orbitales:int = 10
 export var intervalo_spawn:float = 0.8
+
+export var orbital:PackedScene = null
+export(Array, PackedScene) var rutas
 
 onready var impacto_sfx:AudioStreamPlayer2D = $ImpactoSFX
 onready var timer_spawner:Timer = $TimerSpawnerEnemigos
 
 var esta_destruida:bool = false
 var posicion_spawn:Vector2 = Vector2.ZERO
+var ruta_seleccionada:Path2D
  
 func _ready() -> void:
 	timer_spawner.wait_time = intervalo_spawn
 	$AnimationPlayer.play(elegir_animacion_aleatoria())
+	seleccionar_ruta()
 	
 func _process(delta:float) -> void:
 	var player_objetivo:Player = DatosJuego.get_player_actual()
@@ -54,10 +58,10 @@ func destruir() -> void:
 func spawnear_orbital() -> void:
 	numero_orbitales -= 1
 	#var pos_spawn:Vector2 = deteccion_cuadrante()
-	$RutaEnemigo.global_position = global_position
+	ruta_seleccionada.global_position = global_position
 	
 	var new_orbital:EnemigoOrbital = orbital.instance()
-	new_orbital.crear(global_position + posicion_spawn, self, $RutaEnemigo)
+	new_orbital.crear(global_position + posicion_spawn, self, ruta_seleccionada)
 	
 	Eventos.emit_signal("spawn_orbital", new_orbital)
 	
@@ -70,20 +74,26 @@ func deteccion_cuadrante() -> Vector2:
 	var angulo_player:float = rad2deg(dir_player.angle())
 	
 	if abs(angulo_player) <= 45.0:
-		$RutaEnemigo.rotation_degrees = 180.0
+		ruta_seleccionada.rotation_degrees = 180.0
 		return $PosicionesSpawn/Este.position
 	elif abs(angulo_player) > 135.0 and abs(angulo_player) <= 180.0:
-		$RutaEnemigo.rotation_degrees = 0.0
+		ruta_seleccionada.rotation_degrees = 0.0
 		return $PosicionesSpawn/Oeste.position
 	elif abs(angulo_player) > 45.0 and abs(angulo_player) <= 135.0:
 		if sign(angulo_player) > 0:
-			$RutaEnemigo.rotation_degrees = 270.0
+			ruta_seleccionada.rotation_degrees = 270.0
 			return $PosicionesSpawn/Sur.position
 		else:
-			$RutaEnemigo.rotation_degrees = 90.0
+			ruta_seleccionada.rotation_degrees = 90.0
 			return $PosicionesSpawn/Norte.position
 			
 	return $PosicionesSpawn/Norte.position
+	
+func seleccionar_ruta() ->void:
+	randomize()
+	var indice_ruta:int = randi() % rutas.size() -1
+	ruta_seleccionada = rutas[indice_ruta].instance()
+	add_child(ruta_seleccionada)
 
 func _on_AreaColision_body_entered(body: Node) -> void:
 	if body.has_method("destruir"):
--- Juego/Niveles/Nivel.gd
+++ Juego/Niveles/Nivel.gd
@@ -25,6 +25,7 @@ onready var camara_player:Camera2D = $Player/CameraPlayer
 
 #Metodos
 func _ready() -> void:
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	#Eventos.connect("disparo", self, "_on_disparo")
 	player = DatosJuego.get_player_actual()
 	conectar_seniales()
@@ -130,7 +131,15 @@ func contabilizar_bases_enemigas() -> int:
 
 func crear_rele() -> void:
 	var new_rele_masa:ReleMasa = rele_masa.instance()
-	new_rele_masa.global_position = player.global_position + crear_posicion_aleatoria(1000.0, 800.0)
+	#new_rele_masa.global_position = player.global_position + crear_posicion_aleatoria(400.0, 200.0)
+	var pos_aleatoria:Vector2 = crear_posicion_aleatoria(400.0, 200.0)
+	var margen:Vector2 = Vector2(600.0, 600.0)
+	if pos_aleatoria.x < 0:
+		margen.x *= -1
+	if pos_aleatoria.y < 0:
+		margen.y *= -1
+	
+	new_rele_masa.global_position = player.global_position + (margen + pos_aleatoria)
 	add_child(new_rele_masa)
 	
 	
